ed62eacca8d42814bb1438a239516146546adcc6
myleisure <randrianirina.andry@protonmail.com>
2020-03-05 13:08:47 +0100

    Initialize webpack config

---
+++ webpack.common.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js' 
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ],
+    module:{
+        rules: [
+            { test: /\.js$/, use: 'babel-loader'},
+            { test: /\.css$/i, use: ['style-loader','css-loader']}
+        ]
+    }
+}
